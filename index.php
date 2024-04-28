14eaec376ec40ee68120c04d89ad200dfc754960
Natalia Otrombke <nataliaotrombke@gmail.com>
2024-04-28 23:45:36 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+
+<ol>
+  <?php
+  for ($i = 0; $i < 1_000; $i++) {
+    ?>
+      <li>Element #<?php echo $i?></li>
+    <?php
+  }
+  ?>
+</ol>
+
+</body>
+</html>
